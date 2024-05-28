5a39a82ca00ae0a84aef8d09a1570a159ccd6b78
Nick Rakoczy <nickrak.nr@gmail.com>
2024-05-28 09:46:22 -0600

    last kickstart commit, transitioning to plugin files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,4 +7,14 @@ return {
   {
     'pocco81/auto-save.nvim',
   },
+  {
+    'stevearc/oil.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+    init = function()
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end,
+  },
 }
